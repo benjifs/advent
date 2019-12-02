d753e2721fea9254192a0cd6dd6841aac6b6ced6
Benji Encalada Mora <benjifs@users.noreply.github.com>
2019-12-02 12:38:31 -0600

    part2 done

--- 2019/02/day02.go
+++ 2019/02/day02.go
@@ -20,31 +20,51 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+
+		// PART 01
+		input[1] = 12
+		input[2] = 2
 		out, _ := intCode(input)
-		fmt.Println(out[0])
+		fmt.Printf("part 1: %d\n", out[0])
+
+		// PART 02
+		for noun := 0; noun <= 99; noun++ {
+			for verb := 0; verb <= 99; verb++ {
+				input[1] = noun
+				input[2] = verb
+				out, _ := intCode(input)
+
+				if out[0] == 19690720 {
+					fmt.Printf("part 2: %d\n", (100 * noun) + verb)
+					return
+				}
+			}
+		}
 	}
 }
 
 func intCode(input []int) ([]int, error) {
-	for i := 0; i < len(input); i += 4 {
-		op := input[i]
+	mem := make([]int, len(input))
+	copy(mem, input)
+	for i := 0; i < len(mem); i += 4 {
+		op := mem[i]
 
 		if op == 99 {
 			break
 		}
 
-		a := input[i + 1]
-		b := input[i + 2]
-		c := input[i + 3]
+		a := mem[i + 1]
+		b := mem[i + 2]
+		c := mem[i + 3]
 		if op == 1 {
-			input[c] = input[a] + input[b]
+			mem[c] = mem[a] + mem[b]
 		} else if op == 2 {
-			input[c] = input[a] * input[b]
+			mem[c] = mem[a] * mem[b]
 		} else {
 			return nil, errors.New("Invalid op")
 		}
 	}
-	return input, nil
+	return mem, nil
 }
 
 func readInput(filename string) ([]string, error) {
@@ -65,10 +85,8 @@ func readInput(filename string) ([]string, error) {
 }
 
 func inputToInt(input string) ([]int, error) {
-	vals := strings.Split(input, ",")
-
 	var out []int
-	for _, val := range vals {
+	for _, val := range strings.Split(input, ",") {
 		num, err := strconv.Atoi(val)
 		if err != nil {
 			return nil, err
