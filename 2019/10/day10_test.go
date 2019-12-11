9659c13e9e60574392d3d0dcfd2f404ab612100f
Benji Encalada Mora <benjifs@users.noreply.github.com>
2019-12-11 12:33:15 -0600

    adding d10 tests

--- 2019/10/day10_test.go
+++ 2019/10/day10_test.go
@@ -23,8 +23,8 @@ func TestPart1Sample1(t *testing.T) {
 	asteroids := initAsteroids(lines)
 	actual := calculateHits(asteroids)
 
-	if expected != actual.count {
-		t.Errorf("Expected: %d - Actual: %d\n", expected, actual)
+	if expected != len(actual.targets) {
+		t.Errorf("Expected: %d - Actual: %d\n", expected, len(actual.targets))
 	}
 }
 
@@ -35,7 +35,40 @@ func TestPart1Sample2(t *testing.T) {
 	asteroids := initAsteroids(lines)
 	actual := calculateHits(asteroids)
 
-	if expected != actual.count {
+	if expected != len(actual.targets) {
+		t.Errorf("Expected: %d - Actual: %d\n", expected, len(actual.targets))
+	}
+}
+
+func TestSampleFilePt1(t *testing.T) {
+	lines, err := readInput("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	expected := 210
+
+	asteroids := initAsteroids(lines)
+	best := calculateHits(asteroids)
+
+	if expected != len(best.targets) {
+		t.Errorf("Expected: %d - Actual: %d\n", expected, len(best.targets))
+	}
+}
+
+func TestSampleFilePt2(t *testing.T) {
+	lines, err := readInput("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	asteroids := initAsteroids(lines)
+	best := calculateHits(asteroids)
+	destroyed := activateLaser(best)
+
+	expected := 802
+	actual := destroyed[199].x * 100 + destroyed[199].y
+
+	if expected != actual {
 		t.Errorf("Expected: %d - Actual: %d\n", expected, actual)
 	}
 }
+
