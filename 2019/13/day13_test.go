79188593b2d821eaf737f6e615360f2c826592a0
Benji Encalada Mora <benjifs@users.noreply.github.com>
2019-12-13 00:07:11 -0600

    basic tests for day 13

---
+++ 2019/13/day13_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCountBlocks(t *testing.T) {
+	grid := make(map[Point]int)
+	grid[Point{1, 2}] = 3
+	grid[Point{6, 5}] = 4
+	grid[Point{0, 0}] = 1
+	grid[Point{1, 0}] = 2
+	grid[Point{2, 0}] = 3
+	grid[Point{0, 2}] = 2
+	expected := 2
+
+	actual := countBlocks(grid)
+	if expected != actual {
+		t.Errorf("Expected: %d - Actual: %d\n", expected, actual)
+	}
+}
+
