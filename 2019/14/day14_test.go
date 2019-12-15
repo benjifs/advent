7b6dc2dd7dd52975071f6a00f9bdb11073326025
Benji Encalada Mora <benjifs@users.noreply.github.com>
2019-12-14 23:26:39 -0600

    some tests

---
+++ 2019/14/day14_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestParseChemical(t *testing.T) {
+	line := "3 ABC"
+	chemical := parseChemical(line)
+
+	if chemical.quantity != 3 && chemical.name != "ABC" {
+		t.Errorf("Expected 3 ABC\n")
+	}
+}
+
+func TestParseReaction(t *testing.T) {
+	line := "1 A => 5 B"
+	reaction := parseReaction(line)
+	
+	if len(reaction.combination) != 1 {
+		t.Errorf("Incorrect number of combinations\n")
+	}
+
+	line = "1 A, 2 C => 5 B"
+	reaction = parseReaction(line)
+	
+	if len(reaction.combination) != 2 {
+		t.Errorf("Incorrect number of combinations\n")
+	}
+}
+
+func TestParseInput(t *testing.T) {
+	lines := []string{"10 ORE => 10 A", "1 ORE => 1 B", "7 A, 1 B => 1 C", "7 A, 1 C => 1 D", "7 A, 1 D => 1 E", "7 A, 1 E => 1 FUEL"}
+	parsed := parseInput(lines)
+
+	if len(parsed) != 6 {
+		t.Errorf("Incorrect number of reactions\n")
+	}
+}
+
